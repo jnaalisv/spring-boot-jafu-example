b927e4d080a877530ab27c29f92dc7cf68c2d461
Juho Naalisvaara <jnaalisv@hal.local>
2019-01-03 10:48:52 +0200

    bump deps, fix app

--- src/main/java/com/acme/Application.java
+++ src/main/java/com/acme/Application.java
@@ -1,16 +1,17 @@
 package com.acme;
 
-import org.springframework.fu.jafu.ApplicationDsl;
+import org.springframework.fu.jafu.JafuApplication;
 
-import static org.springframework.fu.jafu.ApplicationDsl.application;
+import static org.springframework.fu.jafu.Jafu.webApplication;
+import static org.springframework.fu.jafu.web.WebFluxServerDsl.server;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 public class Application {
 
-    public static ApplicationDsl app = application(app ->
-            app.server(server -> server.router(
+    private static JafuApplication app = webApplication(appConfig ->
+            appConfig.enable(server(serverConfig -> serverConfig.router(
                     route -> route.GET("/", request -> ok().syncBody("Hello world!"))
-            ))
+            )))
     );
 
     public static void main (String[] args) {
