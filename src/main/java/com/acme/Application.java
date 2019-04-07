3c60dff3d6e7fa9213b69607014eaecfdc0b3fcd
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2019-04-07 13:33:47 +0300

    update dependencies

--- src/main/java/com/acme/Application.java
+++ src/main/java/com/acme/Application.java
@@ -1,17 +1,30 @@
 package com.acme;
 
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.fu.jafu.ConfigurationDsl;
 import org.springframework.fu.jafu.JafuApplication;
+import org.springframework.web.reactive.function.server.HandlerFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.fu.jafu.Jafu.webApplication;
-import static org.springframework.fu.jafu.web.WebFluxServerDsl.server;
+import java.util.function.Consumer;
+
+import static org.springframework.fu.jafu.Jafu.reactiveWebApplication;
+import static org.springframework.fu.jafu.webflux.WebFluxServerDsl.webFlux;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 public class Application {
 
-    private static JafuApplication app = webApplication(appConfig ->
-            appConfig.enable(server(serverConfig -> serverConfig.router(
-                    route -> route.GET("/", request -> ok().syncBody("Hello world!"))
-            )))
+    private static ApplicationContextInitializer<GenericApplicationContext> webFluxServer = webFlux(
+            server -> server.router(route -> {
+                route.GET("/", request -> ok().syncBody("Hello world!"));
+            })
+    );
+
+    private static Consumer<ConfigurationDsl> webConf = handle -> handle.enable(webFluxServer);
+
+    private static JafuApplication app = reactiveWebApplication(app ->
+            app.enable(webConf)
     );
 
     public static void main (String[] args) {
