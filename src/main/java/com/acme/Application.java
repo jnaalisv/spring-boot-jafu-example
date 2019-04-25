208b8bd9156bfbb5926b5d7d8a68c65b6765a4e6
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2019-04-25 22:25:26 +0300

    add r2dbc stuff

--- src/main/java/com/acme/Application.java
+++ src/main/java/com/acme/Application.java
@@ -10,14 +10,26 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import java.util.function.Consumer;
 
 import static org.springframework.fu.jafu.Jafu.reactiveWebApplication;
+import static org.springframework.fu.jafu.r2dbc.PostgresqlR2dbcDsl.r2dbcPostgresql;
 import static org.springframework.fu.jafu.webflux.WebFluxServerDsl.webFlux;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 public class Application {
 
+    private static Consumer<ConfigurationDsl> jdbcConf = config
+            -> config.enable(r2dbcPostgresql(pgConfig
+                -> pgConfig
+                    .host("localhost")
+                    .username("admin")
+                    .password("pass")
+        ));
+
+    private static HandlerFunction<ServerResponse> getStuff = request -> ok().syncBody("Hello world!");
+
     private static ApplicationContextInitializer<GenericApplicationContext> webFluxServer = webFlux(
             server -> server.router(route -> {
                 route.GET("/", request -> ok().syncBody("Hello world!"));
+                route.GET("/stuff", getStuff);
             })
     );
 
@@ -25,6 +37,7 @@ public class Application {
 
     private static JafuApplication app = reactiveWebApplication(app ->
             app.enable(webConf)
+                .enable(jdbcConf)
     );
 
     public static void main (String[] args) {
